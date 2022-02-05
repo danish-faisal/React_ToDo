72cb7a75b8f22f8b7fa23d965c6f816abce32d2e
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-02-05 20:47:55 +0530

    Delete functionality added

--- src/App.js
+++ src/App.js
@@ -18,13 +18,24 @@ function App() {
     setTodo("");
   }
 
+  function deleteTodo(todoId) {
+    const updateTodos = [...todos].filter((todo) => todo.id !== todoId);
+
+    setTodos(updateTodos);
+  }
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
         <button type="submit">Add Todo</button>
       </form>
-      {todos.map(todo => <div key={todo.id}>{todo.text}</div>)}
+      {todos.map(todo => (
+        <div>
+          <div key={todo.id}>{todo.text}</div>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
