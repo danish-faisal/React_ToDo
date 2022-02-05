8735115303799629503416206bdd69e1cb6259dd
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-02-05 20:15:32 +0530

    Add ToDo functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("")
+  const [todo, setTodo] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false
+    };
+
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  }
+
   return (
     <div className="App">
-      <form>
-        <input type="text" />
-        <button>Add Todo</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
+        <button type="submit">Add Todo</button>
       </form>
+      {todos.map(todo => <div key={todo.id}>{todo.text}</div>)}
     </div>
   );
 }
